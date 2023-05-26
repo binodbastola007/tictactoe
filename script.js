0b6ecda15974c1b27e1787f833861b0a7b38d18c
binodbastola007 <102938200+binodbastola007@users.noreply.github.com>
2023-05-26 14:47:50 +0545

    Set the width of line to 0

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ const checkWin = () =>{
             gameover = true;
             gameovermusic.play();
             document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "200px";
-            document.querySelector(".line").style.width = "20vw";
+            document.querySelector(".line").style.width = "0vw";
             document.querySelector(".line").style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`;
             
             
